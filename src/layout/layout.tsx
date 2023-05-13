50de0b347c3b82dbbe7205a544930ed6e0315003
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-05-13 17:19:39 +0200

    update style classNames

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout() {
   return (
     <div className="layout">
       <header>
-        <nav className="layout__nav">
+        <nav>
           <Link to="/">Podcaster</Link>
         </nav>
       </header>
