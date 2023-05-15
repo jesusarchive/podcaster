050efcc39dc7f7fb2891aa8fad65b80c2bd4d446
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-05-15 15:46:55 +0200

    add loader component for showing navigation state

---
+++ src/components/ui/loader/loader.tsx
@@ -0,0 +1,12 @@
+import './loader.css';
+
+import React from 'react';
+
+export default function Loader() {
+  return (
+    <div className="loader">
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
--- src/layouts/header/header.tsx
+++ src/layouts/header/header.tsx
@@ -1,13 +1,24 @@
 import './header.css';
 
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigation } from 'react-router-dom';
+
+import Loader from '@/components/ui/loader';
 
 export default function Header() {
+  const { state } = useNavigation();
+  console.log(state);
+
   return (
     <header className="header">
       <nav>
         <Link to="/">Podcaster</Link>
+
+        {state === 'loading' && (
+          <div>
+            <Loader />
+          </div>
+        )}
       </nav>
     </header>
   );
