96111cdf00b1d4034d41189b7707479a1bf8d897
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-05-16 18:33:11 +0200

    add linkify util to replace urls and emails in text with anchor elements

---
+++ src/utils/linkify.ts
@@ -0,0 +1,18 @@
+export function linkify(text: string) {
+  const urlRegex =
+    /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+  const emailRegex = /([\w.]+@[\w.]+\.[\w]+)/g;
+
+  return text
+    .replace(urlRegex, (url) => {
+      let hyperlink = url;
+      if (!hyperlink.match('^https?://')) {
+        hyperlink = `http://${hyperlink}`;
+      }
+
+      return `<a href="${hyperlink}" target="_blank" rel="noopener noreferrer">${url}</a>`;
+    })
+    .replace(emailRegex, (email) => {
+      return `<a href="mailto:${email}">${email}</a>`;
+    });
+}
