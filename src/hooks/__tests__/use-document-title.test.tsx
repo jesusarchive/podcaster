68719ea5a8eb700632256760a9c640a8ba33c334
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-05-17 21:29:21 +0200

    add use document title hook tests

---
+++ src/hooks/__tests__/use-document-title.test.tsx
@@ -0,0 +1,19 @@
+// test use document title hook
+
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import { useDocumentTitle } from '../use-document-title';
+
+const TestComponent = ({ title }: { title: string }) => {
+  useDocumentTitle(title);
+
+  return <div />;
+};
+
+describe('useDocumentTitle', () => {
+  test('should set document title', () => {
+    render(<TestComponent title="test title" />);
+    expect(document.title).toEqual('test title');
+  });
+});
