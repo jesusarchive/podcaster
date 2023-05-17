048bd59bfa6f2db99738ed98237379015af36a8a
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-05-17 13:57:06 +0200

    feat(podcast-list-page): add react-router-dom defer to speed up first load

--- src/pages/podcast-list-page/podcast-list-page.tsx
+++ src/pages/podcast-list-page/podcast-list-page.tsx
@@ -1,7 +1,7 @@
 import './podcast-list-page.css';
 
-import React, { useMemo, useState } from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
+import React, { Fragment, Suspense, useState } from 'react';
+import { Await, defer, Link, useLoaderData } from 'react-router-dom';
 
 import Badge from '@/components/ui/badge';
 import Card from '@/components/ui/card';
@@ -13,50 +13,60 @@ import { filterPodcasts } from './helpers';
 
 // get data from local storage or fetch from API
 export async function podcastListPageLoader() {
-  const podcasts = await getTopPodcastsData();
+  const podcastsPromise = getTopPodcastsData();
 
-  return { podcasts };
+  return defer({ podcastsPromise });
 }
 
 export default function PodcastListPage() {
-  const { podcasts } = useLoaderData() as { podcasts: Array<TopPodcastsFeedEntry> };
+  const { podcastsPromise } = useLoaderData() as { podcastsPromise: Promise<Array<TopPodcastsFeedEntry>> };
   const [search, setSearch] = useState('');
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
   };
 
-  const visiblePodcasts = useMemo(() => filterPodcasts(podcasts, search), [podcasts, search]);
-
   useDocumentTitle('Top Podcasts | Podcaster');
 
   return (
     <main className="podcast-list-page">
-      <article className="search-bar">
-        <Badge>{visiblePodcasts.length}</Badge>
-        <Input placeholder="Filter podcasts..." type="search" value={search} onChange={handleSearchChange} />
-      </article>
-      <article>
-        {visiblePodcasts?.length > 0 ? (
-          <ul>
-            {visiblePodcasts.map((podcast) => (
-              <li key={podcast.id.attributes['im:id']}>
-                <Link to={`/podcast/${podcast.id.attributes['im:id']}`}>
-                  <Card>
-                    <img src={podcast['im:image'][2].label} alt="logo"></img>
-                    <div>
-                      <h2 title={podcast['im:name'].label.toUpperCase()}>{podcast['im:name'].label}</h2>
-                      <h3 title={podcast['im:artist'].label}>{`by ${podcast['im:artist'].label}`}</h3>
-                    </div>
-                  </Card>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <span>No podcasts found.</span>
-        )}
-      </article>
+      <Suspense fallback={<p>Loading top podcasts...</p>}>
+        <Await resolve={podcastsPromise}>
+          {(podcasts) => {
+            const visiblePodcasts = filterPodcasts(podcasts, search);
+
+            return (
+              <Fragment>
+                <article className="search-bar">
+                  <Badge>{visiblePodcasts.length}</Badge>
+                  <Input placeholder="Filter podcasts..." type="search" value={search} onChange={handleSearchChange} />
+                </article>
+                <article>
+                  {visiblePodcasts?.length > 0 ? (
+                    <ul>
+                      {visiblePodcasts.map((podcast) => (
+                        <li key={podcast.id.attributes['im:id']}>
+                          <Link to={`/podcast/${podcast.id.attributes['im:id']}`}>
+                            <Card>
+                              <img src={podcast['im:image'][2].label} alt="logo"></img>
+                              <div>
+                                <h2 title={podcast['im:name'].label.toUpperCase()}>{podcast['im:name'].label}</h2>
+                                <h3 title={podcast['im:artist'].label}>{`by ${podcast['im:artist'].label}`}</h3>
+                              </div>
+                            </Card>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <span>No podcasts found.</span>
+                  )}
+                </article>
+              </Fragment>
+            );
+          }}
+        </Await>
+      </Suspense>
     </main>
   );
 }
