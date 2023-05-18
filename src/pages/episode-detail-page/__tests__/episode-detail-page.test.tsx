8ca1a00523bcd658bb2acf482dd08c338dbc6331
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-05-19 00:44:21 +0200

    clean tests

--- src/pages/episode-detail-page/__tests__/episode-detail-page.test.tsx
+++ src/pages/episode-detail-page/__tests__/episode-detail-page.test.tsx
@@ -2,21 +2,11 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
-import { routesConfig } from '@/routes';
 import podcastLookupResponseMockfrom from '@/services/podcast/__mocks__/podcast-lookup-response-mock.json';
 import topPodcastsResponseMock from '@/services/podcast/__mocks__/top-podcasts-response-mock.json';
 import { getWindow } from '@/utils/testing';
 
 import EpisodeDetailPage, { episodeDetailPageLoader } from '..';
-
-// const loaders = { episodeDetailPageLoader };
-
-// jest.spyOn(loaders, 'episodeDetailPageLoader').mockReturnValue(
-//   Promise.resolve({ podcast: {}, episode: {} }) as Promise<{
-//     podcast: TopPodcastsFeedEntry;
-//     episode: PodcastLookupResult;
-//   }>
-// );
 
 describe('<EpisodeDetailPage />', () => {
   beforeAll(() => {
@@ -25,20 +15,6 @@ describe('<EpisodeDetailPage />', () => {
         json: () => Promise.resolve({})
       })
     );
-  });
-
-  it('renders episode detail page', () => {
-    const router = createBrowserRouter(routesConfig, {
-      window: getWindow('/podcast/1/episode/1'),
-      hydrationData: {
-        loaderData: {
-          podcast: {},
-          episode: {}
-        }
-      }
-    });
-    const { container } = render(<RouterProvider router={router} />);
-    expect(container).toBeTruthy();
   });
 
   it('should call episodeDetailPageLoader', async () => {
@@ -57,7 +33,7 @@ describe('<EpisodeDetailPage />', () => {
     expect(router).toBeTruthy();
   });
 
-  it('new podcast detail page test', () => {
+  it('render with loader data', () => {
     window.__staticRouterHydrationData = {
       loaderData: {
         '0': {
@@ -77,7 +53,7 @@ describe('<EpisodeDetailPage />', () => {
     expect(container).toBeTruthy();
   });
 
-  it('new episode detail page test podcast null', () => {
+  it('render with loader data podcast value null', () => {
     window.__staticRouterHydrationData = {
       loaderData: {
         '0': {
@@ -97,7 +73,7 @@ describe('<EpisodeDetailPage />', () => {
     expect(container).toBeTruthy();
   });
 
-  it('new episode detail page test episode null', () => {
+  it('render with loader data episode value null', () => {
     window.__staticRouterHydrationData = {
       loaderData: {
         '0': {
--- src/pages/podcast-detail-page/__tests__/podcast-detail-page.test.tsx
+++ src/pages/podcast-detail-page/__tests__/podcast-detail-page.test.tsx
@@ -2,21 +2,11 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
-import { routesConfig } from '@/routes';
 import podcastLookupResponseMockfrom from '@/services/podcast/__mocks__/podcast-lookup-response-mock.json';
 import topPodcastsResponseMock from '@/services/podcast/__mocks__/top-podcasts-response-mock.json';
 import { getWindow } from '@/utils/testing';
 
 import PodcastDetailPage, { podcastDetailPageLoader } from '..';
-
-// const loaders = { podcastDetailPageLoader };
-
-// jest.spyOn(loaders, 'podcastDetailPageLoader').mockReturnValue(
-//   Promise.resolve({ podcast: {}, episodes: [] }) as Promise<{
-//     podcast: TopPodcastsFeedEntry;
-//     episodes: Array<PodcastLookupResult>;
-//   }>
-// );
 
 describe('<PodcastDetailPage />', () => {
   beforeAll(() => {
@@ -25,21 +15,6 @@ describe('<PodcastDetailPage />', () => {
         json: () => Promise.resolve({})
       })
     );
-  });
-
-  it('renders podcast detail page', () => {
-    const router = createBrowserRouter(routesConfig, {
-      window: getWindow('/podcast/1'),
-      hydrationData: {
-        loaderData: {
-          podcast: {},
-          episodes: []
-        }
-      }
-    });
-
-    const { container } = render(<RouterProvider router={router} />);
-    expect(container).toBeTruthy();
   });
 
   it('should call podcastDetailPageLoader', async () => {
@@ -58,7 +33,7 @@ describe('<PodcastDetailPage />', () => {
     expect(router).toBeTruthy();
   });
 
-  it('new podcast detail page test', () => {
+  it('render with loader data', () => {
     window.__staticRouterHydrationData = {
       loaderData: {
         '0': {
@@ -78,7 +53,7 @@ describe('<PodcastDetailPage />', () => {
     expect(container).toBeTruthy();
   });
 
-  it('new podcast detail page test podcast null', () => {
+  it('render with loader data podcast value null', () => {
     window.__staticRouterHydrationData = {
       loaderData: {
         '0': {
@@ -98,7 +73,7 @@ describe('<PodcastDetailPage />', () => {
     expect(container).toBeTruthy();
   });
 
-  it('new podcast detail page test episodes empty', () => {
+  it('render with loader data episdoes value empty array', () => {
     window.__staticRouterHydrationData = {
       loaderData: {
         '0': {
