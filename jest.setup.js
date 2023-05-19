b3dd582b8a9a19ba1624a755fffe6c5a3316b9fa
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-05-20 00:34:07 +0200

    jest config mock window scrollTo

--- jest.setup.js
+++ jest.setup.js
@@ -1,3 +1,6 @@
 import '@testing-library/jest-dom/extend-expect';
 
 require('jest-fetch-mock').enableMocks();
+
+const noop = () => {};
+Object.defineProperty(window, 'scrollTo', { value: noop, writable: true });
