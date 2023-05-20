bfe65d2538f83161cd155c70a794664b8bf51660
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-05-20 23:20:08 +0200

    add link to podcast and react memo to podcast detail card shared component

--- src/components/shared/podcast-detail-card/podcast-detail-card.tsx
+++ src/components/shared/podcast-detail-card/podcast-detail-card.tsx
@@ -1,6 +1,7 @@
 import './podcast-detail-card.css';
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Card from '@/components/ui/card';
 import { TopPodcastsFeedEntry } from '@/services/podcast';
@@ -15,24 +16,35 @@ type PodcastDetailCardProps = {
  *
  * Shows podcast detailed information.
  */
-export default function PodcastDetailCard({ podcast }: PodcastDetailCardProps) {
+function PodcastDetailCard({ podcast }: PodcastDetailCardProps) {
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    // Prevent navigation if the user is already in the same page
+    if (event.currentTarget.pathname === window.location.pathname) {
+      event.preventDefault();
+    }
+  };
+
   return podcast && Object.values(podcast).length > 0 ? (
-    <Card className="podcast-detail-card">
-      <div>
-        <img src={podcast['im:image'][2].label} alt="logo"></img>
-      </div>
-      <div>
-        <h2>{podcast['im:name'].label}</h2>
-        <span>{`by ${podcast['im:artist'].label}`}</span>
-      </div>
-      <div>
-        <h3>Description:</h3>
-        <pre
-          dangerouslySetInnerHTML={{
-            __html: linkify(podcast.summary.label)
-          }}
-        />
-      </div>
-    </Card>
+    <Link to={`/podcast/${podcast.id.attributes['im:id']}`} onClick={handleClick}>
+      <Card className="podcast-detail-card">
+        <div>
+          <img src={podcast['im:image'][2].label} alt="logo"></img>
+        </div>
+        <div>
+          <h2>{podcast['im:name'].label}</h2>
+          <span>{`by ${podcast['im:artist'].label}`}</span>
+        </div>
+        <div>
+          <h3>Description:</h3>
+          <pre
+            dangerouslySetInnerHTML={{
+              __html: linkify(podcast.summary.label)
+            }}
+          />
+        </div>
+      </Card>
+    </Link>
   ) : null;
 }
+
+export default React.memo(PodcastDetailCard);
