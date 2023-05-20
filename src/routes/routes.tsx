e3dd6f7c8d1bed18dce43077d83c95f608b07ff6
jesusarchive <jbrdev21880949243098@protonmail.com>
2023-05-20 23:26:36 +0200

    add routes lazy load

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,40 +1,55 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { createBrowserRouter, Outlet, ScrollRestoration } from 'react-router-dom';
 
 import ErrorBoundary from '@/components/error-boundary';
 import Header from '@/layouts/header';
-import EpisodeDetailPage, { episodeDetailPageLoader } from '@/pages/episode-detail-page';
 import NotFoundPage from '@/pages/not-found-page';
-import PodcastDetailPage, { podcastDetailPageLoader } from '@/pages/podcast-detail-page';
-import PodcastList, { podcastListPageLoader } from '@/pages/podcast-list-page';
 
 export const routesConfig = [
   {
     path: '/',
     element: (
-      <>
+      <Fragment>
         <Header />
         <Outlet />
         <ScrollRestoration />
-      </>
+      </Fragment>
     ),
     children: [
       {
         index: true,
-        element: <PodcastList />,
-        loader: podcastListPageLoader,
+        async lazy() {
+          const { podcastListPageLoader, default: PodcastListPage } = await import('@/pages/podcast-list-page');
+
+          return {
+            loader: podcastListPageLoader,
+            Component: PodcastListPage
+          };
+        },
         errorElement: <ErrorBoundary />
       },
       {
         path: 'podcast/:podcastId',
-        element: <PodcastDetailPage />,
-        loader: podcastDetailPageLoader,
+        async lazy() {
+          const { podcastDetailPageLoader, default: PodcastDetailPage } = await import('@/pages/podcast-detail-page');
+
+          return {
+            loader: podcastDetailPageLoader,
+            Component: PodcastDetailPage
+          };
+        },
         errorElement: <ErrorBoundary />
       },
       {
         path: 'podcast/:podcastId/episode/:episodeId',
-        element: <EpisodeDetailPage />,
-        loader: episodeDetailPageLoader,
+        async lazy() {
+          const { episodeDetailPageLoader, default: EpisodeDetailPage } = await import('@/pages/episode-detail-page');
+
+          return {
+            loader: episodeDetailPageLoader,
+            Component: EpisodeDetailPage
+          };
+        },
         errorElement: <ErrorBoundary />
       },
       {
